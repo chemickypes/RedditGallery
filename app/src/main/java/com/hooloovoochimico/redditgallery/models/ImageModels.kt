c1d6f51b776ff18b7d0f67eed97d839c497de017
Angelo Moroni <mor.angelo.mor@gmail.com>
2020-07-30 17:46:07 +0200

    models di reddit e generali

---
+++ app/src/main/java/com/hooloovoochimico/redditgallery/models/ImageModels.kt
@@ -0,0 +1,8 @@
+package com.hooloovoochimico.redditgallery.models
+
+
+data class ImageBean(
+    val url: String? = null,
+    val title: String? = null,
+    val description: String? = null
+)
---
+++ app/src/main/java/com/hooloovoochimico/redditgallery/provider/RedditProvider.kt
@@ -0,0 +1,2 @@
+package com.hooloovoochimico.redditgallery.provider
+
