d1ddcd78a30ab8b48836cdd586fa75fe503d568b
Angelo Moroni <mor.angelo.mor@gmail.com>
2020-07-30 09:58:42 +0200

    debounce per migliorare la ricerca

--- app/src/main/java/com/hooloovoochimico/redditgallery/views/GalleryFragment.kt
+++ app/src/main/java/com/hooloovoochimico/redditgallery/views/GalleryFragment.kt
@@ -2,29 +2,29 @@ package com.hooloovoochimico.redditgallery.views
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
-import android.view.ViewGroup
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.widget.SearchView
-import androidx.core.content.ContextCompat
-import androidx.core.view.MenuItemCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import com.hooloovoochimico.redditgallery.R
 import com.hooloovoochimico.redditgallery.adapter.GridAdapter
 import com.hooloovoochimico.redditgallery.viewmodels.GalleryViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.fragment_gallery.*
+import java.util.concurrent.TimeUnit
 
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
     private val galleryViewModel: GalleryViewModel by viewModels()
 
     private val gridAdapter = GridAdapter()
+
+    private val searchSubject = PublishSubject.create<String>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -37,13 +37,13 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
         })
 
         galleryViewModel.loading.observe(viewLifecycleOwner, Observer {
-           if(it) Log.d(javaClass.name, "loading")
+            if (it) Log.d(javaClass.name, "loading")
         })
 
         galleryViewModel.error.observe(viewLifecycleOwner, Observer {
-           if(it) {
-               Log.d(javaClass.name, "error")
-           }
+            if (it) {
+                Log.d(javaClass.name, "error")
+            }
         })
 
         list.apply {
@@ -54,6 +54,16 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
     }
 
     private fun configureSearch() {
+
+        val d = searchSubject
+            .debounce(300, TimeUnit.MILLISECONDS)
+            .subscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                galleryViewModel.search(it)
+            }
+
+
         val searchItem: MenuItem? = toolabr.menu.findItem(R.id.action_search)
         if (searchItem != null) {
             val searchView = searchItem.actionView as SearchView
@@ -65,15 +75,15 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
             val searchPlateView: View =
                 searchView.findViewById(androidx.appcompat.R.id.search_plate)
 
-            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
                 override fun onQueryTextSubmit(query: String?): Boolean {
                     return true
                 }
 
                 override fun onQueryTextChange(newText: String?): Boolean {
 
-                    if (newText?.isNotBlank() == true || newText?.isNotEmpty() == true){
-                        galleryViewModel.search(newText)
+                    if (newText?.isNotBlank() == true || newText?.isNotEmpty() == true) {
+                        searchSubject.onNext(newText)
                     }
 
                     return true
