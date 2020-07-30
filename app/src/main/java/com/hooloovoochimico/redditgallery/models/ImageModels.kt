f3857496515b5ad403ad8907918faec05384f3ea
Angelo Moroni <mor.angelo.mor@gmail.com>
2020-07-30 17:46:35 +0200

    migliorato model immagine

--- app/src/main/java/com/hooloovoochimico/redditgallery/models/ImageModels.kt
+++ app/src/main/java/com/hooloovoochimico/redditgallery/models/ImageModels.kt
@@ -4,5 +4,6 @@ package com.hooloovoochimico.redditgallery.models
 data class ImageBean(
     val url: String? = null,
     val title: String? = null,
-    val description: String? = null
+    val description: String? = null,
+    val id: String? = null
 )
